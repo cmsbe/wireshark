47f0ccf8721d5776001c89cf2b7d836db9f6ef98
Eyobiel-12 <125456316+Eyobiel-12@users.noreply.github.com>
2023-05-18 15:45:28 +0200

    Update see.py

--- see.py
+++ see.py
@@ -1,77 +1,76 @@
 import tkinter as tk
 import random
+import datetime
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        #dit is de titel van onze programma
         self.title("Eindopdracht - Opdracht 6")
-        #configuratie om het achtergrond van onze programma aan te passen, wij hebben hem een blauwe kleur gegeven
         self.configure(background="blue")
 
-        #wij maken drie frames aan, elke opdracht heeft zijn eigen frame
-        self.frames = [] #wij maken een lijst voor de frames
-        self.create_frame("BTW berekening", self.calculate_btw, "Voer een bedrag in:") #label voor btw berekening, gemaakt door Eyobil
-        self.create_frame("Reistijd berekening", self.calculate_reistijd, "Voer de afstand in (km):", "Voer de snelheid in (km/h):") #label voor reistijd berekening, gemaakt door David
-        self.create_frame("Vierkant berekening", self.calculate_vierkant, "Voer de lengte in (m):", "Voer de breedte in (m):") #label voor vierkant meter berekening, gemaakt door Musabe
-        self.create_frame("Getal raden", self.guess_number, "Voer een minimumwaarde in:", "Voer een maximumwaarde in:") #label voor willeukeurig getal, gemaakt door Serhat
+        self.frames = {}
+        self.result_frames = {}
+        self.create_frame("BTW berekening", self.calculate_btw, "Voer een bedrag in:")
+        self.create_frame("Reistijd berekening", self.calculate_reistijd, "Voer de afstand in (km):", "Voer de snelheid in (km/h):")
+        self.create_frame("Leeftijd berekening", self.calculate_age, "Voer je geboortejaar in:")
+        self.create_frame("Getal raden", self.guess_number, "Voer een minimumwaarde in:", "Voer een maximumwaarde in:")
 
-    #wij maken een def aan om de frames aan te maken, wij geven hem create_frame als naam
     def create_frame(self, title, command, *labels):
-        frame = tk.Frame(self) #wij maken een frame aan
-        frame.pack(padx=10, pady=10) #wij voegen marges van 10 pixels op elke kant van het vierkant
-        self.frames.append(frame) #de frame wordt toegevoegd in de lijst frames
+        main_frame = tk.Frame(self, width=700, height=200, padx=10, pady=10)  # Larger main frame
+        main_frame.pack(fill='both', expand=True, padx=20, pady=20)
 
-        label1 = tk.Label(frame, text=title) #maken van een label waarin de titel van daarnet in komt
-        label1.pack() #voegt text toe in het label en toon aan op scherm, de methode pack zorgt ervoor dat het op het scherm getoont word
+        frame = tk.Frame(main_frame, width=350)  # Left side for inputs
+        frame.pack(side='left', fill='both', expand=True)
+        self.frames[command] = frame
 
-        entries = [] #lijst aanmaken
-        for l in labels: #voor elke l in labels wordt een label gemaakt en toegevoegd
-            label = tk.Label(frame, text=l) #frame aangemaakt
-            label.pack() #word getoond op scherm
-            entry = tk.Entry(frame) #entry gebruiken voor binnen het frame zodat tekst en getallen geaccepteerd kunnen worden, het verzamelen van tekst en getallen van de gebruiker
-            entry.pack() #word getoond op scherm
-            entries.append(entry) #de frame wordt toegevoegd in de lijst frames
+        label1 = tk.Label(frame, text=title)
+        label1.pack()
 
-        button = tk.Button(frame, text="Bereken", command=lambda: command(*entries)) #creeeren button met tekst bereken, command is een callback, wanneer je op de button drukt wordt de functie command aangeroept
-        button.pack(pady=5) #voegen van 5 pixels toe onder de knop
+        entries = []
+        for l in labels:
+            label = tk.Label(frame, text=l)
+            label.pack()
+            entry = tk.Entry(frame)
+            entry.pack()
+            entries.append(entry)
 
-        result_label = tk.Label(frame, text="") #lege label, resultaten gaan hier later getoond worden
-        result_label.pack()
+        button = tk.Button(frame, text="Bereken", command=lambda: command(*entries))
+        button.pack(pady=5)
 
-    #def om btw te berekenen
+        result_frame = tk.Frame(main_frame, width=350)  # Right side for results
+        result_frame.pack(side='bottom', fill='both', expand=True)
+        self.result_frames[command] = result_frame
+
     def calculate_btw(self, entry):
-        bedrag = float(entry.get()) #float gebruikt om decimalen te kunnen zien en get om de getal te kunnen krijgen
+        bedrag = float(entry.get())
         btw = bedrag * 0.21
-        self.result(btw, "BTW")
+        self.result(self.calculate_btw, btw, "BTW")
 
-    #def om de reistijd te berekenen
     def calculate_reistijd(self, entry1, entry2):
         afstand = float(entry1.get())
         snelheid = float(entry2.get())
         reistijd = afstand / snelheid
-        self.result(reistijd, "Reistijd", "uur")
+        self.result(self.calculate_reistijd, reistijd, "Reistijd", "uur")
 
-    #def om de oppervlakte te berekenen
-    def calculate_vierkant(self, entry1, entry2):
-        lengte = float(entry1.get())
-        breedte = float(entry2.get())
-        omtrek = 2 * (lengte + breedte)
-        oppervlakte = lengte * breedte
-        self.result(omtrek, "Omtrek", "\nOppervlakte", oppervlakte)
+    def calculate_age(self, entry):
+        birth_year = int(entry.get())
+        current_year = datetime.datetime.now().year  # Get the current year
+        age = current_year - birth_year
+        self.result(self.calculate_age, age, "Je leeftijd")
 
     def guess_number(self, entry1, entry2):
         min_value = int(entry1.get())
         max_value = int(entry2.get())
         random_number = random.randint(min_value, max_value)
-        self.result(random_number, "Willekeurig getal")
+        self.result(self.guess_number, random_number, "Willekeurig getal")
 
-    #hier zetten wij alle labels om de resultaten te tonen
-    def result(self, val1, label1, val2="", label2=""):
-        result_label = tk.Label(self.frames[-1], text=label1 + ": " + str(val1) + val2 + "\n" + label2 + ": " + str(label2))
+    def result(self, command, val1, label1, val2=None, label2=None):
+        result_text = label1 + ": " + str(val1)
+        if val2 is not None and label2 is not None:  # Check if val2 and label2 are given
+            result_text += "\n" + label2 + ": " + str(val2)
+        result_label = tk.Label(self.result_frames[command], text=result_text)
         result_label.pack()
 
-
-if __name__ == "__main__": #instantie maken
+if __name__ == "__main__":
     app = App()
-    app.mainloop() #start mainloop van de applicatie
+    app.mainloop()
